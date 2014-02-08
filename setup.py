11add6cd23d606c60afb4d83ac056233822ee51c
Aron Jones <aron.jones@gmail.com>
2014-02-08 13:15:00 -0500

    make the setup.py file py3 compatible

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 from setuptools import setup
 import re
 import os
@@ -49,9 +51,9 @@ version = get_version(package)
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist upload")
     args = {'version': version}
-    print "You probably want to also tag the version now:"
-    print "  git tag -a %(version)s -m 'version %(version)s'" % args
-    print "  git push --tags"
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
     sys.exit()
 
 
